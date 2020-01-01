9fc039279f249f5cfddc622c84fc0bbe9273f72a
Samuel Zandi <samuelczandi@gmail.com>
2020-01-01 21:31:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-{:key => value}
+{key => value}
 end
 
 def read_from_hash(hash, key)
@@ -36,5 +36,6 @@ def update_counting_hash(hash, key)
     hash[key] += 1
   else hash[key] = 1
   end
+  hash
 end
 
